805f04e4b8f43f4256f63f671b1c69784b4f7153
ngmthang-g <hyanrymine3@gmail.com>
2026-09-17 01:11:52 +0700

    test: add failing LD process classifier stub

---
+++ src/AutoGiaLapLD/Core/LDProcessClassifier.cs
@@ -0,0 +1,10 @@
+namespace AutoGiaLapLD.Core
+{
+    internal static class LDProcessClassifier
+    {
+        public static bool IsLDPlayerWindow(string processName, string windowTitle, string className)
+        {
+            return false;
+        }
+    }
+}
